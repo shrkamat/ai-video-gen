a6fe7db4501fc05ab061d3330306e52e70afb2c4
Shrinivas Kamath <skamath@synamedia.com>
2024-05-16 22:25:54 +0530

    video stich with blanking

--- stich.py
+++ stich.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, CompositeVideoClip
 import json
 from pathlib import Path
 
@@ -31,19 +31,94 @@ def create_summary(video_file, segIds, audio_tok_file):
 
     subclips = []
     input_video = VideoFileClip(video_file)
+    black_video = VideoFileClip("blackVideo.mp4")
+    black_video_clip = black_video.subclip(0, 1)
+    black_video_clip.set_fps(60)
+    counter = 0
+    continuation = False
+    start = 0
+    total_regions = len(segIds)
+    print(str(total_regions))
+    for setId in segIds:
+        last_region = segIds[-1] == setId
+        segment = data[setId]
+        end_this = segment["end"]
+        start_next = 0
 
-    for region in segIds:
-        segment = data[region]
-        start = segment["start"] - 0.500
-        end = segment["end"] + 0.500
+        further_continuation = False
+        if not last_region:
+            # print(data[(regions[counter+1])])
+            start_next = data[(segIds[counter+1])]["start"]
+            # further_continuation = regions[counter] == (regions[counter+1]-1)
+            print("start_next="+str(start_next)+",end_this="+str(end_this))
+            further_continuation = start_next - end_this < 1.0
+        else:
+            further_continuation = False
 
-        print(str(start)+"-->"+str(end)+": \t"+segment["text"])
-        subclip = input_video.subclip(start, end)
-        subclips.append(subclip)
+        print("i="+str(counter)+":"+str(setId)+",last_region="+str(last_region),
+              "further_continuation="+str(further_continuation)+",continuation="+str(continuation))
+
+        if last_region:
+            if continuation:
+                print("cond:1.1")
+                end = segment["end"]
+                print(str(start)+"-->"+str(end)+": \t"+segment["text"])
+                subclip = input_video.subclip(start, end)
+                subclips.append(subclip)
+            else:
+                print("cond:1.2")
+                start = segment["start"] - 0.500
+                end = segment["end"]
+                print(str(start)+"-->"+str(end)+": \t"+segment["text"])
+                subclip = input_video.subclip(start, end)
+                subclips.append(subclip)
+            continuation = False
+        elif continuation:
+            if further_continuation:
+                print("cond:2.1 = this is middle clip no operation will be merged fully")
+            else:
+                print("cond:2.2 = no further continuation clip")
+                end = segment["end"]
+                subclip = input_video.subclip(start, end)
+                print(str(start)+"-->"+str(end)+": \t"+segment["text"])
+                subclips.append(subclip)
+                continuation = False
+        elif further_continuation:
+            print("cond:3 new continuation found")
+            start = segment["start"] - 0.500
+            continuation = True
+        else:
+            print("cond:4 no continuation")
+            start = segment["start"] - 0.500
+            end = segment["end"] + 0.500
+            print(str(start)+"-->"+str(end)+": \t"+segment["text"])
+            subclip = input_video.subclip(start, end)
+            subclips.append(subclip)
+            continuation = False
+
+        # start = segment["start"]- 0.500
+        # end = segment["end"] + 0.500
+        # subclip = input_video.subclip(segment["start"], segment["end"])
+
+        counter += 1
+
+    fade_time = 0.5
+    video_clips = subclips
+    video_fx_list = [video_clips[0]]
+
+    idx = video_clips[0].duration - fade_time
+    for video in video_clips[1:]:
+        print("idx="+str(idx))
+        video_fx_list.append(black_video_clip.set_start(
+            idx).crossfadein(fade_time))
+        idx += black_video_clip.duration - fade_time
+        video_fx_list.append(video.set_start(idx).crossfadein(fade_time))
+        idx += video.duration - fade_time
 
     # Closing file
     tokenized_subtitles.close()
-    return concatenate_videoclips(subclips)
+    # return concatenate_videoclips(subclips)
+    return CompositeVideoClip(video_fx_list)
 
 
 def create_summary_video(session):
@@ -53,7 +128,7 @@ def create_summary_video(session):
     summary.to_videofile(
         output_path,
         codec="libx264",
-        temp_audiofile="temp.m4a", remove_temp=True, audio_codec="aac")
+        temp_audiofile="temp.m4a", remove_temp=True, audio_codec="aac", fps=60)
 
 
 if __name__ == "__main__":
