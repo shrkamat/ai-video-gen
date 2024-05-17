9bbf4a581efaf8b17d6d71637e575a37e3c10cd3
Shrinivas Kamath <skamath@gmail.com>
2024-05-17 17:22:04 +0530

    can't use use Python3.8 in all colab instances

--- system-init.sh
+++ system-init.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# set up python 3.8 (google colab)
-sudo apt-get update -y
-sudo apt-get install python3.8 python3.8-distutils
-from IPython.display import clear_output 
-clear_output()
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+set -xe
 
-# Choose one of the given alternatives:
-sudo update-alternatives --config python3
+cmd="pip install \
+    moviepy yt-dlp \
+    ffmpeg-python \
+    openai \
+    nvidia_smi"
 
-# This one used to work but now NOT(for me)!
-# !sudo update-alternatives --config python
-
-# Check the result
-python3 --version
-
-# Attention: Install pip (... needed!)
-sudo apt install python3-pip
+if `python -c "import whisperx"` ; then
+    echo "whisperx is already installed, check if it can be upgraded"
+    pip install git+https://github.com/m-bain/whisperx.git --upgrade
+else
+    echo "first time install"
+    cmd="$cmd git+https://github.com/m-bain/whisperx.git"
+    $cmd
+fi
